41bf7c6e797850cbaa51f571e5b4197e0e117fac
vv68 <vv68@bk.ru>
2024-08-11 18:05:59 +0300

    init

---
+++ frontend/config/main.php
@@ -0,0 +1,60 @@
+<?php
+
+use modules\books\BooksModule;
+
+$params = array_merge(
+    require __DIR__ . '/../../common/config/params.php',
+    require __DIR__ . '/../../common/config/params-local.php',
+    require __DIR__ . '/params.php',
+    require __DIR__ . '/params-local.php'
+);
+
+return [
+    'id' => 'app-frontend',
+    'name' => 'Books catalog',
+    'basePath' => dirname(__DIR__),
+    'language' => 'ru-RU',
+    'bootstrap' => ['log', 'books'],
+    'controllerNamespace' => 'frontend\controllers',
+    'components' => [
+        'request' => [
+            'csrfParam' => '_csrf-frontend',
+        ],
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+            'identityCookie' => ['name' => '_identity-frontend', 'httpOnly' => true],
+        ],
+        'session' => [
+            // this is the name of the session cookie used for login on the frontend
+            'name' => 'advanced-frontend',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => \yii\log\FileTarget::class,
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                ['pattern' => '/', 'route' => 'books/book/index']
+            ],
+        ],
+
+    ],
+    'params' => $params,
+    'modules' => [
+        'books' => BooksModule::class,
+        'controllerNamespace' => 'modules\books\controllers',
+        'viewPath' => '@modules/books/views',
+    ]
+];
