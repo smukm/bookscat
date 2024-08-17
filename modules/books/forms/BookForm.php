2dc5ecd14458af9850e03c566b98200c0c4d93fe
vv68 <vv68@bk.ru>
2024-08-17 17:58:58 +0300

    isbn validator as class

--- modules/books/forms/BookForm.php
+++ modules/books/forms/BookForm.php
@@ -7,6 +7,7 @@ namespace modules\books\forms;
 use InvalidArgumentException;
 use modules\books\entities\Author;
 use modules\books\entities\Book;
+use modules\books\forms\validators\IsbnValidator;
 use Yii;
 use yii\base\Model;
 use yii\web\UploadedFile;
@@ -59,7 +60,7 @@ class BookForm extends Model
             [['isbn'], 'trim'],
             [['isbn'], 'string'],
             [['isbn'], 'required'],
-            [['isbn'], 'validateIsbn'],
+            [['isbn'], IsbnValidator::class],
             [
                 ['isbn'],
                 'unique',
@@ -114,7 +115,7 @@ class BookForm extends Model
         }
     }
 
-    public function validateIsbn($attribute): void
+    /*public function validateIsbn($attribute): void
     {
         $isbn = $this->$attribute;
         $isbn = preg_replace('#\D#', '', $isbn);
@@ -135,35 +136,7 @@ class BookForm extends Model
         } catch (InvalidArgumentException $ex) {
             $this->addError($attribute, $ex->getMessage());
         }
-    }
+    }*/
 
-    private function check10Isbn(string $isbn): bool
-    {
-        $digits = str_split($isbn);
 
-        $sum = 0;
-
-        foreach ($digits as $index => $digit) {
-
-            $sum += ($index + 1) * (int)$digit;
-        }
-
-        return ($sum % 11) === 0;
-    }
-
-    private function check13Isbn(string $isbn): bool
-    {
-        $digits = str_split($isbn);
-
-        $check = 0;
-        for ($i = 0; $i < 13; $i += 2) {
-            $check += (int)$digits[$i];
-        }
-
-        for ($i = 1; $i < 12; $i += 2) {
-            $check += 3 * $digits[$i];
-        }
-
-        return ($check % 10) === 0;
-    }
 }
